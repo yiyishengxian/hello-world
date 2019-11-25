b3aa83734cc1c972915a1c83c9eabaf7089dec80
yiyishengxian <55731844+yiyishengxian@users.noreply.github.com>
2019-11-25 20:02:48 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -12,7 +12,7 @@ int main(void)
     res=curl_easy_perform(curl);
     if(res!=CURLE_OK)
       fprintf(stderr,"curl_easy_perform() failed:%s\n",
-            curl_easy_stror(res));
+            curl_easy_strerror(res));
       curl_easy_cleanup(curl);
    }
    return 0;
